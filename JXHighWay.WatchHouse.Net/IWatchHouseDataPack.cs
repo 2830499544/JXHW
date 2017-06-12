aedcc2336f0786e6d32ab11939107c423f9087cd
YouXiangMing <allayxm@hotmail.com>
2017-06-12 19:45:12 +0800

    增加了岗亭的数据包头部分的接口

--- JXHighWay.WatchHouse.Net/IWatchHouseDataPack.cs
+++ JXHighWay.WatchHouse.Net/IWatchHouseDataPack.cs
@@ -8,9 +8,57 @@ namespace JXHighWay.WatchHouse.Net
 {
     interface IWatchHouseDataPack
     {
+        /// <summary>
+        /// 头
+        /// </summary>
+         byte Head { get; set; }
+        /// <summary>
+        /// 信息长度
+        /// </summary>
+         short Length { get; set; }
+        /// <summary>
+        /// 协议版本号
+        /// </summary>
+         short Ver { get; set; }
+        /// <summary>
+        /// 登陆序号
+        /// </summary>
+         short LoginSN { get; set; }
+        /// <summary>
+        /// 序列号
+        /// </summary>
+         short SN { get; set; }
+        /// <summary>
+        /// 目标岗亭唯一ID
+        /// </summary>
+         int WatchHouseID { get; set; }
+        /// <summary>
+        /// 用户唯一ID
+        /// </summary>
+         int UserID { get; set; }
+        /// <summary>
+        /// 预留字段
+        /// </summary>
+         int Empty { get; set; }
+        /// <summary>
+        /// 智慧系列设备
+        /// </summary>
          byte ID_H { get; set; }
+        /// <summary>
+        /// ID
+        /// </summary>
          byte ID_L { get; set; }
+        /// <summary>
+        /// 命令
+        /// </summary>
          byte CMD { get; set; }
+        /// <summary>
+        /// 子命令
+        /// </summary>
          byte SUB { get; set; }
+        /// <summary>
+        /// 校验
+        /// </summary>
+        short Check { get; set; }
     }
 }
--- JXHighWay.WatchHouse.Net/WatchHouseDataPack_Receive_DoorGuard.cs
+++ JXHighWay.WatchHouse.Net/WatchHouseDataPack_Receive_DoorGuard.cs
@@ -11,6 +11,38 @@ namespace JXHighWay.WatchHouse.Net
     /// </summary>
     struct WatchHouseDataPack_Receive_DoorGuard:IWatchHouseDataPack
     {
+        /// <summary>
+        /// 头
+        /// </summary>
+         public byte Head { get; set; }
+        /// <summary>
+        /// 信息长度
+        /// </summary>
+         public short Length { get; set; }
+        /// <summary>
+        /// 协议版本号
+        /// </summary>
+         public short Ver { get; set; }
+        /// <summary>
+        /// 登陆序号
+        /// </summary>
+         public short LoginSN { get; set; }
+        /// <summary>
+        /// 序列号
+        /// </summary>
+         public short SN { get; set; }
+        /// <summary>
+        /// 目标岗亭唯一ID
+        /// </summary>
+         public int WatchHouseID { get; set; }
+        /// <summary>
+        /// 用户唯一ID
+        /// </summary>
+         public int UserID { get; set; }
+        /// <summary>
+        /// 预留字段
+        /// </summary>
+        int Empty { get; set; }
         public byte ID_H { get; set; }
         public byte ID_L { get; set; }
         public byte CMD { get; set; }
